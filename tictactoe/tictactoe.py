097e4f46284595a26cebb70ac07d6ab830f92fc6
Ramanathan M <rus.cahimb@gmail.com>
2016-09-14 21:16:12 +0530

    CLI based tic-tac-toe, winner prediction, grid redraw pending

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -5,11 +5,12 @@
 
 from random import randrange
 from itertools import repeat
+from time import sleep
 
-grid_marks = { 1: ["C1", "C2", "C3"],
-               2: ["C4", "C5", "C6"],
-               3: ["C7", "C8", "C9"]
-             }
+
+grid_marks = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"]
+total_cells = 9
+cells_used = []
 
 
 
@@ -38,8 +39,13 @@ def redrawGrid(cells_used):
 	print("\n\n")
 	row_delim = "=" * 20
 	col_delim = "||"
+	for g in uxip:
+	    usedcell = "C" + str(g)
+	    print("Cell, ", usedcell + " is at index position in grid marks : ", end=' == ')
+	    print(grid_marks.index(usedcell), end=" ")
+	    grid_marks[grid_marks.index(usedcell)] = "X"
+
 	num = 1
-	cnine = repeat("C", 9)
 	for i in range(1,5):
 		print(row_delim + "\n")
 		if i != 4:
@@ -50,6 +56,7 @@ def redrawGrid(cells_used):
 			print("\n")
 
 	return None
+
 
 
 def showGrid(input=''):
@@ -76,6 +83,80 @@ def showGrid(input=''):
 
 
 
+def userAPlay():
+
+	"""
+	"""
+
+	user_choice = input("^^" * 5 + " User A: Enter a valid cell number [1-9]: ")
+	if validNum(int(user_choice)):
+		print("User's choice was : " + "C" + user_choice)
+		cells_used.append("C" + user_choice)
+		print("Cell, C", user_choice + " is at index position in grid : ", end=' == ')
+		print(grid_marks.index("C" + user_choice)+1, end=" ")
+		print("\n")
+		grid_marks[grid_marks.index("C" + user_choice)] = "R"
+		print("Valid cells available are : ", grid_marks)
+		print("\n")
+#		redrawGrid(cells_used)
+#		winnerStatus()
+
+	return 
+
+
+
+def userDPlay():
+
+	"""
+	"""
+
+	user_choice = input("^^" * 5 + " User D: Enter a valid cell number [1-9]: ")
+	if validNum(int(user_choice)):
+		print("User's choice was : " + "C" + user_choice)
+		cells_used.append("C" + user_choice)
+		print("Cell, C", user_choice + " is at index position in grid : ", end=' == ')
+		print(grid_marks.index("C" + user_choice)+1, end=" ")
+		print("\n")
+		grid_marks[grid_marks.index("C" + user_choice)] = "D"
+		print("Valid cells available are : ", grid_marks)
+		print("\n")
+#		redrawGrid(cells_used)
+#		winnerStatus()
+
+	return 
+
+
+
+""" On hold until we find a solution for:
+how shall we retain the numbers already used and feed 
+that to random() such that when the turn of the 
+computer arrives, different cells are chosen ? 
+"""
+def compPlay():
+
+	"""
+	"""
+
+	print("++" * 5 + " Now it's the turn of the computer, allow R2D2 to think (ah,ah,ah) . . .\n")
+	sleep(5)	
+	comp_choice = randrange(1,10,1)
+	if validNum(comp_choice):
+		comp_choice = str(comp_choice)
+		print("Computer's choice was : " + "C" + comp_choice)
+		cells_used.append("C" + comp_choice)
+		print("Cell, C", comp_choice + " is at index position in grid : ", end=' == ')
+		print(grid_marks.index("C" + comp_choice)+1, end=" ")
+		print("\n")
+		grid_marks[grid_marks.index("C" + comp_choice)] = "D"
+		print("Valid cells available are : ", grid_marks)
+		print("\n")
+#		redrawGrid(cells_used)
+#		winnerStatus()
+
+	return 
+
+
+
 def main():
 
 	"""
@@ -83,30 +164,26 @@ def main():
 
 	# help menu
 
-	total_cells = 9
-	cells_used = []
 
 	# start fresh game
 	if len(cells_used) == 0:
-		print("=" * 2 + "Let's start a new game ... remember cells are numbered to help you\n")
-		print("=" * 2 + "When game starts, C1, C3 markings will indicate empty cells\n)
+		print("==" * 10 + "  Welcome to the game of Tic-Tac-Toe  " + "==" * 10 + "\n")
+		print("=" * 5 + " Remember cells are numbered to help you\n")
+		print("=" * 5 + " And, C1...C9 markings indicate empty cells")
 		showGrid()
 
-	user_choice = input("Enter a valid cell number [0-9]: ")
-#	comp_choice = randrange(1,10,1)
-
-	if len(cells_used) <= total_cells:
-
-	if validNum(int(user_choice)):
-		print("User's choice was : " + "C" + user_choice)
-		cells_used.append("C" + user_choice)
-		redrawGrid(cells_used)
+	while (total_cells-len(cells_used) != 0):
+		print("$&" * 5 + " {} free cells are available in the grid".format(total_cells-len(cells_used)))
+		userAPlay()
+		if len(cells_used) == total_cells:
+			print("/\\/\\" * 5 + " Game over buddies!!!! " + "/\\/\\" * 5 + "\n\n")
+			exit(0)
+		else:
+			print("$&" * 5 + " {} free cells are available in the grid".format(total_cells-len(cells_used)))
+			userDPlay()
+#		compPlay()
 	else:
-		if validNum(int(user_choice)):
-			print("User's choice was : " + "C" + user_choice)
-			cells_used.append("C" + choice)
-
-
+		print("Game over buddies!!!!", end=' == ')
 
 	# Based on valid input, fill the grid
 	# show the updated grid and prompt the
